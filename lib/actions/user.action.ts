706b10bb71180d881666bc88838e2910dc63f3aa
Codemaverick07 <hemantjatal59@gmail.com>
2024-03-05 00:16:38 +0530

    Add svix package and update user actions

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -2,6 +2,13 @@
 
 import User from "@/database/user.model";
 import { connectToDatabase } from "../mongoose";
+import {
+  CreateUserParams,
+  DeleteUserParams,
+  UpdateUserParams,
+} from "./shared.types";
+import { revalidatePath } from "next/cache";
+import Question from "@/database/question.model";
 
 export async function getUserById(params: any) {
   try {
@@ -9,6 +16,50 @@ export async function getUserById(params: any) {
     const { userId } = params;
     const user = await User.findOne({ clerkId: userId });
     return user;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function createUser(userData: CreateUserParams) {
+  try {
+    connectToDatabase();
+    const newUser = await User.create(userData);
+    return newUser;
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function updateUser(params: UpdateUserParams) {
+  try {
+    const { clerkId, updateData, path } = params;
+    connectToDatabase();
+    await User.findOneAndUpdate({ clerkId }, updateData, {
+      new: true,
+    });
+
+    revalidatePath(path);
+  } catch (error) {
+    console.log(error);
+
+    throw error;
+  }
+}
+export async function deleteUser(params: DeleteUserParams) {
+  try {
+    const { clerkId } = params;
+    connectToDatabase();
+    const user = await User.findOneAndDelete({ clerkId });
+    if (!user) {
+      throw new Error("user dose not exits");
+    }
+    await Question.find({ author: user._id }).distinct("_id");
+    await Question.deleteMany({ author: user._id });
+    const deletedUser = await User.findByIdAndDelete(user._id);
+    return deletedUser;
   } catch (error) {
     console.log(error);
 
