20e53414e9be60bb7c09cae6b32dcefb23717bf6
Codemaverick07 <hemantjatal59@gmail.com>
2024-03-06 21:40:44 +0530

    Add getAllUsers function to user.action.ts and community page

---
+++ components/cards/UserCard.tsx
@@ -0,0 +1,58 @@
+import { getTopInteractedTags } from "@/lib/actions/tags.action";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+import { Badge } from "../ui/badge";
+import RenderTag from "../shared/RenderTag";
+interface props {
+  user: {
+    _id: string;
+    clerkId: string;
+    picture: string;
+    name: string;
+    username: string;
+  };
+}
+
+const UserCard = async ({ user }: props) => {
+  const interactedTags = await getTopInteractedTags({ userId: user._id });
+  return (
+    <Link
+      href={`/profile/${user.clerkId}`}
+      className=" shadow-light100_darknone w-full max-xs:min-w-full xs:w-[260px]"
+    >
+      <article className="flex w-full flex-col items-center justify-center rounded-2xl border p-8">
+        <Image
+          src={user.picture}
+          alt="user profile picture"
+          width={100}
+          height={100}
+          className="rounded-full"
+        />
+        <div className="mt-4">
+          <h3 className="h3-bold text-dark200_light900 line-clamp-1">
+            {user.name}
+          </h3>
+          <p className="body-regular text-dark500_light500 mt-2">
+            @{user.username}
+          </p>
+        </div>
+        <div className="mt-5">
+          {interactedTags.length > 0 ? (
+            <div>
+              {interactedTags.map((tag) => {
+                return (
+                  <RenderTag key={tag._id} _id={tag._id} name={tag.name} />
+                );
+              })}
+            </div>
+          ) : (
+            <Badge>No tags yet</Badge>
+          )}
+        </div>
+      </article>
+    </Link>
+  );
+};
+
+export default UserCard;
